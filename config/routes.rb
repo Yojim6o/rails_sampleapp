997ea83858c5b00a7084aab4c62ab46d023f8cf2
Jimmy <jgs2017@gmail.com>
2015-10-22 15:17:09 -0500

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   #get 'static_pages/../..'
@@ -15,6 +17,12 @@ Rails.application.routes.draw do
   get 'contact' =>  'static_pages#contact'
 
   get 'signup'  =>  'users#new'
+
+  get 'login'   =>  'sessions#new'
+
+  post 'login'   =>  'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
 
